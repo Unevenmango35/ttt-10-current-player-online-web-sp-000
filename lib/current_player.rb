3acf1b60c2787881d32cca2e2ac696f4c0c751db
Rosie Choi <rosiechoimanso@gmail.com>
2020-06-12 20:26:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,10 @@
+
 def turn_count(board)
   counter = 0
-if board.each do |count|
-  count = "X" || "O"
-  counter += 1
-return counter  
-end
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
 end
